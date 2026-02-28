975b2d13728130f20d54000e55914d00f83847ad
Caporal2024 <caporalpcs6@gmail.com>
2026-02-28 10:09:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="/static/casino.css">
-</head>
-<body>
+class UltraSafeMode:
+    def __init__(self, balance):
+        self.start_balance = balance
+        self.balance = balance
+        self.bet = balance * 0.05
+        self.stop_loss = balance * 0.90
+        self.stop_gain = balance * 1.15
+        self.consecutive_losses = 0
+        self.max_losses = 2
 
-<div class="header">
-<div>🍏 APPLE</div>
-</div>
+    def update_balance(self, new_balance):
+        self.balance = new_balance
 
-<div class="card">
-<button onclick="play()">CHOISIR</button>
-</div>
+        if self.balance <= self.stop_loss:
+            return "STOP_LOSS"
 
-<script>
-function play(){
-let win = Math.random()<0.7;
-if(win){
-alert("🍏 GAGNÉ !");
-}else{
-alert("❌ PERDU");
-}
-}
-</script>
+        if self.balance >= self.stop_gain:
+            return "STOP_GAIN"
 
-</body>
-</html>
+        return "CONTINUE"
+
+    def register_result(self, win):
+        if win:
+            self.consecutive_losses = 0
+        else:
+            self.consecutive_losses += 1
+
+        if self.consecutive_losses >= self.max_losses:
+            return "STOP_2_LOSSES"
+
+        return "CONTINUE"
+
+    def get_bet_amount(self):
+        return round(self.bet, 2)
