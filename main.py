1f29cfca0d8bacdd0d4629a0e6e461c5b4cb7eb5
Caporal2024 <caporalpcs6@gmail.com>
2026-03-02 20:40:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
+PORT = int(os.environ.get("PORT", 8000))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot démarré avec succès.")
+    await update.message.reply_text("✅ Bot Railway fonctionne !")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-print("Bot en cours de démarrage...")
-app.run_polling()
+print("Bot démarré...")
+
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    webhook_url=f"https://{os.environ.get('RAILWAY_STATIC_URL')}"
+)
