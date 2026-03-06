6b93de9686d6cbf4f692db02f4b9b4e6a72cba63
Caporal2024 <caporalpcs6@gmail.com>
2026-03-06 02:07:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 ACCESS_CODE = "CAPORAL123"
 users = {}
+player_ids = {}
 
 def menu():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -25,27 +26,37 @@ def menu():
 def start(message):
     bot.send_message(
         message.chat.id,
-        "🚀 Bienvenue dans le BOT SIGNAL AVIATOR\n\n🔐 Clique sur Login pour accéder.",
+        "🚀 Bienvenue dans le BOT SIGNAL AVIATOR",
         reply_markup=menu()
     )
 
 
+# LOGIN
 @bot.message_handler(func=lambda message: message.text == "🔑 Login")
 def login(message):
-    bot.send_message(message.chat.id, "🔑 Envoie le code d'accès.")
+    bot.send_message(message.chat.id, "Send Player ID")
 
 
+# SAVE PLAYER ID
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def save_player_id(message):
+    player_ids[message.chat.id] = message.text
+    bot.send_message(message.chat.id, "✅ Player ID saved")
+
+
+# ACCESS CODE
 @bot.message_handler(func=lambda message: message.text == ACCESS_CODE)
 def access_granted(message):
     users[message.chat.id] = True
-    bot.send_message(message.chat.id, "✅ Accès autorisé. Les signaux sont débloqués.")
+    bot.send_message(message.chat.id, "✅ Access granted. Signals unlocked.")
 
 
+# SIGNAL
 @bot.message_handler(func=lambda message: message.text == "⏳ Next Signal")
 def signal(message):
 
-    if users.get(message.chat.id) != True:
-        bot.send_message(message.chat.id, "❌ Accès refusé. Clique sur Login.")
+    if message.chat.id not in player_ids:
+        bot.send_message(message.chat.id, "⚠️ Send Player ID first.")
         return
 
     entree = round(random.uniform(1.20, 1.60), 2)
@@ -65,11 +76,13 @@ def signal(message):
     bot.send_message(message.chat.id, msg)
 
 
+# OPEN GAME
 @bot.message_handler(func=lambda message: message.text == "🎮 Open Game")
 def open_game(message):
     bot.send_message(message.chat.id, "🎮 https://spribe.co/games/aviator")
 
 
+# HISTORY
 @bot.message_handler(func=lambda message: message.text == "📊 Odds History")
 def history(message):
     bot.send_message(message.chat.id, "📊 Fonction historique bientôt disponible.")
