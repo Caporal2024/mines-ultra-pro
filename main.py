3457b3ad562569bde775c63373decd47877e27bf
Caporal2024 <caporalpcs6@gmail.com>
2026-03-06 00:42:36 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import telebot
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "🤖 Bot actif !")
+
+print("Bot démarré...")
+bot.infinity_polling()
