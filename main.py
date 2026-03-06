25df08da56b014e057bafa6b011460e409e69f88
Caporal2024 <caporalpcs6@gmail.com>
2026-03-06 01:52:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 users = {}
 
-# Générer signal Aviator
 def aviator_signal():
-
     entree = round(random.uniform(1.20,1.70),2)
     cashout = round(random.uniform(2.00,3.50),2)
 
@@ -22,7 +20,6 @@ def aviator_signal():
 💰 Cashout : {cashout}x
 """
 
-# START
 @bot.message_handler(commands=['start'])
 def start(message):
 
@@ -38,14 +35,11 @@ def start(message):
         reply_markup=keyboard
     )
 
-
-# Messages
 @bot.message_handler(func=lambda message: True)
 def messages(message):
 
     chat = message.chat.id
 
-    # LOGIN
     if message.text == "🔑 Login":
 
         bot.send_message(
@@ -56,21 +50,17 @@ def messages(message):
         users[chat] = "waiting_id"
         return
 
-
-    # Player ID
     if users.get(chat) == "waiting_id":
 
         users[chat] = True
 
         bot.send_message(
             chat,
-            "✅ Player ID enregistré.\nTu peux maintenant recevoir les signaux."
+            "✅ Player ID enregistré."
         )
 
         return
 
-
-    # SIGNAL
     if message.text == "🚀 Next Signal":
 
         if users.get(chat) != True:
@@ -79,15 +69,12 @@ def messages(message):
                 chat,
                 "⚠️ Tu dois d'abord envoyer ton Player ID avec 🔑 Login."
             )
-
             return
 
-        bot.send_message(chat,"⏳ Analyse du signal...")
+        bot.send_message(chat,"⏳ Analyse...")
         time.sleep(2)
 
         bot.send_message(chat, aviator_signal())
 
-
 print("Bot lancé")
-
 bot.infinity_polling()
